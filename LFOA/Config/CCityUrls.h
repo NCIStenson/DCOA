f56df4328af1327d4c8a1d342df573d1c338839c
Stenson <609974752@qq.com>
2020-04-17 09:57:27 +0800

    修改服务器地址

--- LFOA/Config/CCityUrls.h
+++ LFOA/Config/CCityUrls.h
@@ -12,9 +12,11 @@
 //http://122.226.81.198:8083/dcyd  公司
 //http://60.10.185.43:8088/dcyd    用户
 
-// http://60.10.184.35:18088/dcyd  // 客户现场
+// http://60.10.184.35:18088/dcyd  // 旧版本客户服务器地址
+// http://222.222.75.43:18088/dcyd // 2020年4月5号更换服务器地址
 
 //#define CCITY_BASE_URL @"http://122.226.81.198:8082/dcyd"
-#define CCITY_BASE_URL @"http://60.10.184.35:18088/dcyd"
+//#define CCITY_BASE_URL @"http://60.10.184.35:18088/dcyd"
+#define CCITY_BASE_URL @"http://222.222.75.43:18088/dcyd"
 
 #endif /* CCityUrls_h */
